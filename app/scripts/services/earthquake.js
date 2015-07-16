dc26aa74b7a469c63a61644479acbba0238b6be7
mathias <mathiasd88@gmail.com>
2015-07-16 03:12:39 -0300

    update

--- app/scripts/services/earthquake.js
+++ app/scripts/services/earthquake.js
@@ -1,5 +1,5 @@
 'use strict';
 
 app.factory('Earthquake', ['$resource', function ($resource) {
-	return $resource('http://earthquake.usgs.gov/fdsnws/event/1/query');
+	return $resource('http://earthquake.usgs.gov/fdsnws/event/1/query/');
 }]);
